f506cbad8771aa45885af0ce084398d838051ecb
xnm-sdksdk <n.mendonca@protonmail.com>
2025-01-25 14:07:51 +0000

    new imports

--- imports.py
+++ imports.py
@@ -1,9 +1,11 @@
 # imports
 import tkinter as tk
-from tkinter import ttk, messagebox, simpledialog
+from tkinter import ttk, messagebox, simpledialog, filedialog
+from tkinter.filedialog import askopenfile
 import os
 import uuid
 import datetime
 from time import strftime
+from PIL import Image, ImageTk
 
-__all__ = ['tk', 'ttk', 'messagebox', 'simpledialog', 'os', 'uuid', 'datetime', 'strftime']
+__all__ = ['tk', 'ttk', 'messagebox', 'simpledialog', 'os', 'uuid', 'datetime', 'strftime', 'filedialog', 'askopenfile', 'Image', 'ImageTk']
